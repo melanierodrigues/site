b75ccf86d7c3fd2c4cd865d4c7cc7d379bdffbc0
Melanie Rodrigues <hello@melanierodrigues.com>
2023-03-17 21:17:50 +0000

    work on skills Column Component

--- src/components/SkillsColumn.vue
+++ src/components/SkillsColumn.vue
@@ -1,62 +1,60 @@
 <template>
+    <!--| | SKILLS COLUMN | |-->
     <div class="skills-column-wrapper">
         <!-- Left Column -->
         <div class="skills-left-column-wrapper">
             <div
-            v-for="text in props.left"
+            v-for="text in props.columnData"
             class="left-column"
+            :class="{ 'text-selected-color': selected === text.id }"
             :key="text.id"
-            @click="emit('leftColumnClicked', $event)">
-            {{ text.text }}
+            @click="[selectedTextRight = text.textRight, selected = text.id]">
+            {{ text.textLeft }}
             </div>
         </div>
-
         <!-- Divider -->
         <div class="divider" />
-
         <!-- Right Column -->
         <div class="skills-right-column-wrapper">
-            <div
-            v-for="text in props.left"
-            class="right-column"
-            :key="text.id"
-            @click="emit('leftColumnClicked', $event)">
-            {{ text.text }}
-            </div>
+            <div class="right-column">{{ selectedTextRight }}</div>
         </div>
     </div>
 </template>
   
 <script setup>  
 /* Vue */
-//   import { ref } from 'vue'
-
-/* Emmitters */
-const emit = defineEmits(['leftColumnClicked'])
+import { ref, onMounted } from 'vue'
 
 /** Props received:
-@LeftColumn { Array } - each item is an Object
+@columnData { Array } - each item is an Object
   - id { String }
-  - text { String }
+  - textLeft { String }
+  - textRight { String }
 */
 const props = defineProps({
-    left: Array
+    columnData: Array
 })
 
-/*************************************************
- *                                                *
- *             Design System Detail               *
- *                                                *
- *************************************************/
+/* Selectors Data */
+const selected = ref() 
+const selectedTextRight = ref('')
+
+onMounted(() => {
+    selected.value = props.columnData[0].id
+    selectedTextRight.value = props.columnData[0].textRight
+})
 </script>
 
 <style lang="scss" scoped>
 .skills-column-wrapper {
     display: flex;
     flex-direction: row;
-    gap: 50px;
+    gap: 32px;
+
+    animation: fade-in-fwd 0.6s cubic-bezier(0.390, 0.575, 0.565, 1.000);
 
     .divider {
+        flex-grow: 0;
         border: 1px solid var(--main-white);
     }
     .skills-left-column-wrapper {
@@ -65,6 +63,29 @@ const props = defineProps({
         gap: 8px;
         .left-column {
             color: var(--main-white);
+            opacity: 0.2;
+            font-size: 18px;
+            font-weight: 400;
+            line-height: 26px;
+            text-transform: capitalize;
+
+            &:hover {
+                cursor: pointer;
+                animation: fade-hover 0.4s cubic-bezier(0.390, 0.575, 0.565, 1.000) forwards;
+            }
+        }
+
+        .text-selected-color {
+            opacity: 1;
+        }
+    }
+
+    .skills-right-column-wrapper {
+        display: flex;
+        flex-direction: column;
+        gap: 3px;
+        .right-column {
+            color: var(--main-white);
             font-size: 18px;
             font-weight: 400;
             line-height: 26px;
@@ -72,16 +93,23 @@ const props = defineProps({
         }
     }
 
-    .skills-right-column-wrapper {
-        display: flex;
-        flex-direction: column;
-        gap: 8px;
-        .right-column {
-            color: var(--main-white);
-            font-size: 18px;
-            font-weight: 400;
-            line-height: 26px;
-            text-transform: capitalize;
+    @keyframes fade-in-fwd {
+        0% {
+            transform: translateZ(-80px);
+            opacity: 0;
+        }
+        100% {
+            transform: translateZ(0);
+            opacity: 1;
+        }
+    }
+
+    @keyframes fade-hover {
+        0% {
+            opacity: 0.2;
+        }
+        100% {
+            opacity: 0.4;
         }
     }
 }
--- src/designSystem/SkillsColumnDS.vue
+++ src/designSystem/SkillsColumnDS.vue
@@ -1,61 +1,42 @@
 <template>
-  <skills-column :left="leftColumn"/>
+  <!-- *********************************************
+  *                                                *
+  *	                Skills Column	                 *
+  *                                                *
+  ********************************************** -->
+  <skills-column :columnData="columnData"/>
 </template>
   
 <script setup>
 import SkillsColumn from '../components/SkillsColumn.vue'
-/* Vue */
-// import { ref } from 'vue'
 
 /* i18n */
 import { useI18n } from 'vue-i18n'
 const { t } = useI18n()
 
-/* Emmitters */
-// const emit = defineEmits(['leftColumnClicked'])
-
-const teste = () => {
-  alert(`Isto é um teste`)
-}
-const leftColumn = [
+const columnData = [
 	{
 		id: 1,
-    text: t('aboutMe.skills.programming'),
-		route: '../designSystem/ColorsPalette.vue',
-    haveCopyImport: false,
-    clicked: teste
+    textLeft: t('aboutMe.skills.programming'),
+    textRight: t('aboutMe.skills.programmingTextRight')
 	}, {
 		id: 2,
-    text: t('aboutMe.skills.tools'),
-		route: '../designSystem/SkillsColumnDS.vue',
-    haveCopyImport: false,
-    clicked: teste
+    textLeft: t('aboutMe.skills.tools'),
+    textRight: t('aboutMe.skills.toolsTextRight')
 	}, {
 		id: 3,
-    text: t('aboutMe.skills.design'),
-		route: '../designSystem/ComponentA.vue',
-    haveCopyImport: false,
-    clicked: teste
+    textLeft: t('aboutMe.skills.design'),
+    textRight: t('aboutMe.skills.designTextRight')
 	}, {
 		id: 4,
-    text: t('aboutMe.skills.methodologies'),
-		route: '../designSystem/ComponentA.vue',
-    haveCopyImport: false,
-    clicked: teste
+    textLeft: t('aboutMe.skills.methodologies'),
+    textRight: t('aboutMe.skills.methodologiesTextRight')
 	}, {
 		id: 5,
     text: t('aboutMe.skills.languages'),
-		route: '../designSystem/ComponentA.vue',
-    haveCopyImport: false,
-    clicked: teste
+    textRight: t('aboutMe.skills.languagesTextRight')
 	}
 ]
-
-/*************************************************
-*                                                *
-*             Design System Detail               *
-*                                                *
-*************************************************/
 </script>
 
 <style lang="scss" scoped>
