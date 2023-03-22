71ed72ceb54dbc0f106b2d2b5ea0b52d67c25dff
Melanie Rodrigues <hello@melanierodrigues.com>
2023-03-22 20:15:49 +0000

    work on About Me

---
+++ src/views/mainScroll/MainScroll.vue
@@ -0,0 +1,19 @@
+<template>
+  <about-me />
+</template>
+  
+<script setup>
+import AboutMe from '../../components/mainScroll/AboutMe.vue';
+/* i18n */
+// import { useI18n } from 'vue-i18n'
+// const { t } = useI18n()
+/*************************************************
+*                                                *
+*                  Hello World                   *
+*                                                *
+*************************************************/
+</script>
+
+<style lang="scss" scoped>
+</style>
+  
